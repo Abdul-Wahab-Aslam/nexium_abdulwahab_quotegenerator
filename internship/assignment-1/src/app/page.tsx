006fe642c0b269a3880e0f38c75d56a567991bbd
Abdul Wahab Aslam <133736431+Abdul-Wahab-Aslam@users.noreply.github.com>
2025-07-08 23:57:02 +0500

    Add files via upload

---
+++ internship/assignment-1/src/app/page.tsx
@@ -0,0 +1,21 @@
+
+
+// import QuoteGenerator from "@/components/QuoteGenerator";
+
+// export default function Home() {
+//   return (
+//     <main className="min-h-screen flex items-center justify-center bg-base-200">
+//       <QuoteGenerator />
+//     </main>
+//   );
+// }
+
+import QuoteGenerator from "@/components/QuoteGenerator";
+
+export default function Home() {
+  return (
+    <main>
+      <QuoteGenerator />
+    </main>
+  );
+}
